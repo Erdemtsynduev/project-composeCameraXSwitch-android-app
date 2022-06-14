684788417050ff0fca2c718d018e63ac2bcb6007
Erdem Tsynduev <erdemtsynduev@gmail.com>
2022-06-15 01:19:21 +0300

    Feat: Add simple camera view.

--- app/src/main/java/com/erdemtsynduev/composecameraxswitch/MainActivity.kt
+++ app/src/main/java/com/erdemtsynduev/composecameraxswitch/MainActivity.kt
@@ -1,43 +1,60 @@
 package com.erdemtsynduev.composecameraxswitch
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.erdemtsynduev.composecameraxswitch.camerax.SimpleCameraPreview
 import com.erdemtsynduev.composecameraxswitch.ui.theme.ComposeCameraXSwitchTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            setViewContent()
+        } else {
+            showErrorPermissionToast()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermissionsCamera()
+    }
+
+    private fun checkPermissionsCamera() {
+        if (permissionsCameraGranted()) {
+            setViewContent()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun showErrorPermissionToast() {
+        Toast.makeText(
+            this,
+            getString(R.string.permission_message),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun setViewContent() {
         setContent {
             ComposeCameraXSwitchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                SimpleCameraPreview({
+
+                })
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCameraXSwitchTheme {
-        Greeting("Android")
-    }
+    private fun permissionsCameraGranted() = ContextCompat.checkSelfPermission(
+        baseContext, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
 }
